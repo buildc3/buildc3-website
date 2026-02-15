d6b484fc93994bd5ac61889e45ca7f87ade80b24
Robom <robom.915@gmail.com>
2026-02-15 22:23:01 +0530

    fix - remove unnecessary div wrapper around Spline component for cleaner layout.

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -3,9 +3,7 @@ import Spline from '@splinetool/react-spline';
 const Homepage = () => {
   return (
     <main className="h-screen w-screen overflow-hidden">
-      <div className="h-full w-full" style={{ transform: 'translateY(5%)', transformOrigin: 'center center' }}>
-        <Spline scene="https://prod.spline.design/0cyEg1aPz0Si15tV/scene.splinecode" />
-      </div>
+      <Spline scene="https://prod.spline.design/0cyEg1aPz0Si15tV/scene.splinecode" />
     </main>
   );
 };
